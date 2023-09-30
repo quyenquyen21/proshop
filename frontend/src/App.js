61dd0ca5eb409bb1c4951a9c3c34a14c1d32f971
Tuong Quyen Dao <daotuongquyen@gmail.com>
2023-09-30 13:02:13 -0400

    Add header, footer and customize boostrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 const App = () => {
-  return <h1>Welcome to Pro shop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Pro shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
