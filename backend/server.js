709b5c3631a11913192df9352926de0ac12e114b
Tuong Quyen Dao <daotuongquyen@gmail.com>
2023-10-09 16:32:39 -0400

    Authorized user

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 connectDB();
 const app = express();
+
+//Body parse middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
