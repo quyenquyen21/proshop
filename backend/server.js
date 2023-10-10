48ef1ae03c49288be1baf90ba0e4b0a73aaef7fb
Tuong Quyen Dao <daotuongquyen@gmail.com>
2023-10-10 15:03:39 -0400

    Order routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 connectDB();
 const app = express();
 
@@ -22,6 +23,8 @@ app.get('/', (req, res) => {
 });
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
